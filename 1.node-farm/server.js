df6b5641baeb8aa542d144c5c32b0d7126ffced7
004 <anbschool0004@004ui-Macmini.local>
2023-09-25 19:19:44 +0700

    node

--- 1.node-farm/server.js
+++ 1.node-farm/server.js
@@ -1,7 +1,31 @@
 const http = require('http');
+const path = require('path');
+const url = require('url')
+const fs = require('fs');
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    res.end('Hello from the server!');
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview'){
+        res.end('This is the overview');
+    }
+    else if(pathName === '/product'){
+        res.end('This is the PRODUCT');
+    }
+    else if (pathName === '/api'){
+        res.writeHead(200,{'content-type' : 'application/json'});
+        res.end(data);
+    }
+    else{
+        res.writeHead(404,{
+            'Content-type' : 'text/html',
+            'my-own-header' : 'hello-world'
+        });
+        res.end('<h1> Page not found!!! </h1>');
+    }
 });
 
 // 8000 is port : sub address on a certain host
