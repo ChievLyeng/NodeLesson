280643424700eeb24eceb7b01d6ec195a997140d
004 <anbschool0004@004ui-Macmini.local>
2023-09-29 19:55:37 +0700

    29_9

--- 1.node-farm/server.js
+++ 1.node-farm/server.js
@@ -14,7 +14,8 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`,'ut
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`,'utf-8');
 const dataObj = JSON.parse(data);
 
-console.log(slugify("Fresh Avocado",{lower : true}));
+const slugs = dataObj.map( el => slugify(el.productName, {lower : true}));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
     
